523aff99ab8c1f950b716b9a43adf9ae4b692308
M Haji <mshaji89@gmail.com>
2014-12-31 09:02:15 +0000

    parsing path

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 var http = require("http");
+var url = require("url");
 
-    function start(){
-        function onRequest(request, response){
-            response.writeHead(200, {"Content-Type" : "text/plain"});
-            response.write("Hello world");
-            var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress || 
-                      request.socket.remoteAddress || request.connection.socket.remoteAddress;
-            console.log("Request received from " + ip);
-            
-            response.end();
-        }
+function start(){
+    function onRequest(request, response){
+        var path = url.parse(request.url).pathname;
+        response.writeHead(200, {"Content-Type" : "text/plain"});
+        response.write("Hello world");
+        var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress || 
+                  request.socket.remoteAddress || request.connection.socket.remoteAddress;
+        console.log("Request received for " + path + " from " + ip);
         
-        http.createServer(onRequest).listen(1337);
-   }
+        response.end();
+    }
+    
+    http.createServer(onRequest).listen(1337);
+}
 
 exports.start = start;
