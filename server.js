95ea2b9496abd7177438967a573a2b0175a94a76
M Haji <mshaji89@gmail.com>
2014-12-31 09:30:31 +0000

    wiring router

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var http = require("http");
 var url = require("url");
 
-function start(){
+function start(route){
     function onRequest(request, response){
         var path = url.parse(request.url).pathname;
-        response.writeHead(200, {"Content-Type" : "text/plain"});
-        response.write("Hello world");
         var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress || 
                   request.socket.remoteAddress || request.connection.socket.remoteAddress;
         console.log("Request received for " + path + " from " + ip);
+       
+        route(path);
         
+        response.writeHead(200, {"Content-Type" : "text/plain"});
+        response.write("Hello world");
         response.end();
     }
     
